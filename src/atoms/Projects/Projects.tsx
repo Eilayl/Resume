bda26b76aa22003b9a911bc5c4f4450a259f5e6a
unknown <eilaylevi95@gmail.com>
2025-10-03 17:00:43 +0300

    change projects

--- src/atoms/Projects/Projects.tsx
+++ src/atoms/Projects/Projects.tsx
@@ -1,7 +1,7 @@
 import { ProjectItem } from '../ProjectItem/ProjectItem'
 import './Projects.css'
 import deliFront from '../../assets/DeliFront.png'
-
+import dynamicMachine from '../../assets/DynamicMachine.png'
 export const Projects = () => {
   return (
     <div className="projects-container">
@@ -14,34 +14,11 @@ export const Projects = () => {
           link="https://delifront-1.onrender.com/"
         />
         <ProjectItem
-          title="Portfolio"
-          description="Personal Website"
-          img={deliFront}
-          link="https://example.com"
-        />
-        <ProjectItem
-          title="Shop"
-          description="E-Commerce Platform"
-          img={deliFront}
-          link="https://example.com"
-        />
-        <ProjectItem
-          title="Shop"
-          description="E-Commerce Platform"
-          img={deliFront}
-          link="https://example.com"
-        />
-        <ProjectItem
-          title="Shop"
-          description="E-Commerce Platform"
-          img={deliFront}
-          link="https://example.com"
-        />
-        <ProjectItem
-          title="Shop"
-          description="E-Commerce Platform"
-          img={deliFront}
-          link="https://example.com"
+          title="Dynamic Table"
+          description="Dynamic table, that allowed to the developer easily add/remove variables by only 1 line
+          the frontend puill the data from api and render it dynamically"
+          img={dynamicMachine}
+          link="https://dynamic-machine-frontend.onrender.com/"
         />
       </div>
     </div>
